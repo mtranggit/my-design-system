1ac05cd1796522d716401ddf48c09751c7aaa077
Michael Trang <michael.trang@hotmail.com>
2026-05-05 00:18:35 +1000

    remove prefix

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps
 export function Button({ children, ...other }: ButtonProps): JSX.Element {
   return (
     <button type="button" {...other}>
-      MT {children}
+      {children}
     </button>
   );
 }
